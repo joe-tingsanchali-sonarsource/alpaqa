9140d744affca725f1b8caab2e27882d05694663
Pieter Pas <pieter.p.dev@outlook.com>
2024-03-30 16:09:20 +0100

    [Py] set LIBRARY_OUTPUT_DIRECTORY for _alpaqa module

--- interfaces/python/cmake/Pybind11Stubgen.cmake
+++ interfaces/python/cmake/Pybind11Stubgen.cmake
@@ -1,6 +1,8 @@
 function(pybind11_stubgen target)
 
     find_package(Python3 REQUIRED COMPONENTS Interpreter)
+    set_target_properties(${target} PROPERTIES
+        LIBRARY_OUTPUT_DIRECTORY "$<CONFIG>/${PY_BUILD_CMAKE_MODULE_NAME}")
     add_custom_command(TARGET ${target} POST_BUILD
         COMMAND ${Python3_EXECUTABLE} -m pybind11_stubgen
                 ${PY_BUILD_CMAKE_MODULE_NAME}.$<TARGET_FILE_BASE_NAME:${target}>
