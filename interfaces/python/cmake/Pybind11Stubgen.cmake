8038291b4a8dc800b0026ed2a6dbb6401945fb41
Pieter Pas <pieter.p.dev@outlook.com>
2024-03-30 01:11:26 +0100

    [Py] improve stub generation

--- interfaces/python/cmake/Pybind11Stubgen.cmake
+++ interfaces/python/cmake/Pybind11Stubgen.cmake
@@ -3,19 +3,19 @@ function(pybind11_stubgen target)
     find_package(Python3 REQUIRED COMPONENTS Interpreter)
     add_custom_command(TARGET ${target} POST_BUILD
         COMMAND ${Python3_EXECUTABLE} -m pybind11_stubgen
-                $<TARGET_FILE_BASE_NAME:${target}>
+                ${PY_BUILD_CMAKE_MODULE_NAME}.$<TARGET_FILE_BASE_NAME:${target}>
                 --numpy-array-use-type-var
                 --exit-code
                 --enum-class-locations Sign:LBFGS
                 -o ${CMAKE_CURRENT_BINARY_DIR}
-        WORKING_DIRECTORY $<TARGET_FILE_DIR:${target}>
+        WORKING_DIRECTORY $<TARGET_FILE_DIR:${target}>/..
         USES_TERMINAL)
 
 endfunction()
 
 function(pybind11_stubgen_install target destination)
 
-    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/$<TARGET_FILE_BASE_NAME:${target}>/
+    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${PY_BUILD_CMAKE_MODULE_NAME}/$<TARGET_FILE_BASE_NAME:${target}>/
         EXCLUDE_FROM_ALL
         COMPONENT python_stubs
         DESTINATION ${destination}/$<TARGET_FILE_BASE_NAME:${target}>
