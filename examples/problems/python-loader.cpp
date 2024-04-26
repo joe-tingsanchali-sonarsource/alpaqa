cb090a4ac2c02ce80193a59bd1520ab70b60adf3
Pieter Pas <pieter.pas@outlook.com>
2024-04-26 22:23:55 +0200

    Add python-loader DLProblem

---
+++ examples/problems/python-loader.cpp
@@ -0,0 +1,410 @@
+#include <pybind11/detail/common.h>
+#include <python-loader/export.h>
+
+#include <alpaqa/config/config.hpp>
+#include <alpaqa/dl/dl-problem.h>
+#include <alpaqa/params/params.hpp>
+#include <alpaqa/util/string-util.hpp>
+
+#include <span>
+#include <stdexcept>
+#include <string_view>
+#include <utility>
+
+#include <pybind11/eigen.h>
+#include <pybind11/embed.h>
+#include <pybind11/stl.h>
+namespace py = pybind11;
+
+namespace alpaqa {
+
+struct PYTHON_LOADER_EXPORT python_loader_error : std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
+
+namespace {
+
+USING_ALPAQA_CONFIG(DefaultConfig);
+
+struct PYTHON_LOADER_NO_EXPORT Problem {
+    alpaqa_problem_functions_t funcs{};
+    py::scoped_interpreter python_interpreter;
+    std::string ref;
+    std::string name;
+    struct ObjWrapper {
+        std::optional<py::object> o;
+        operator py::object &() { return o.value(); }
+        operator const py::object &() const { return o.value(); }
+        ObjWrapper() = default;
+        ObjWrapper &operator=(py::object &&o) {
+            this->o = std::move(o);
+            return *this;
+        }
+        ObjWrapper(const ObjWrapper &)            = delete;
+        ObjWrapper &operator=(const ObjWrapper &) = delete;
+        ~ObjWrapper() {
+            if (o) {
+                py::gil_scoped_acquire gil;
+                o.reset();
+            }
+        }
+        template <class... Args>
+        decltype(auto) attr(Args &&...args) {
+            return o.value().attr(std::forward<Args>(args)...);
+        }
+    } o;
+
+    Problem(std::string_view ref, std::span<const std::string_view> opts)
+        : ref{ref} {
+        py::gil_scoped_acquire gil;
+        try {
+            auto [mod_name, attr_name] = alpaqa::util::split(ref, ":");
+            auto mod = py::module_::import(std::string(mod_name).c_str());
+            std::string attr_name_str{attr_name};
+            auto constructor = mod.attr(attr_name_str.c_str());
+            py::list constr_args;
+            for (auto a : opts)
+                constr_args.append(py::str(a));
+            o    = constructor(constr_args);
+            name = std::string{py::str(o)} + " (from " + this->ref + ")";
+
+            using P = Problem;
+            using alpaqa::member_caller;
+            funcs.n                = py::cast<length_t>(o.attr("n"));
+            funcs.m                = py::cast<length_t>(o.attr("m"));
+            funcs.name             = name.c_str();
+            funcs.eval_f           = member_caller<&P::eval_f>();
+            funcs.eval_grad_f      = member_caller<&P::eval_grad_f>();
+            funcs.eval_g           = member_caller<&P::eval_g>();
+            funcs.eval_grad_g_prod = member_caller<&P::eval_grad_g_prod>();
+            // clang-format off
+            if (py::hasattr(o, "eval_proj_diff_g"))
+                funcs.eval_proj_diff_g = member_caller<&P::eval_proj_diff_g>();
+            if (py::hasattr(o, "eval_proj_multipliers"))
+                funcs.eval_proj_multipliers = member_caller<&P::eval_proj_multipliers>();
+            if (py::hasattr(o, "eval_prox_grad_step"))
+                funcs.eval_prox_grad_step = member_caller<&P::eval_prox_grad_step>();
+            if (py::hasattr(o, "eval_inactive_indices_res_lna"))
+                funcs.eval_inactive_indices_res_lna = member_caller<&P::eval_inactive_indices_res_lna>();
+            if (py::hasattr(o, "eval_jac_g"))
+                funcs.eval_jac_g = member_caller<&P::eval_jac_g>();
+            if (py::hasattr(o, "eval_grad_gi"))
+                funcs.eval_grad_gi = member_caller<&P::eval_grad_gi>();
+            if (py::hasattr(o, "eval_f_grad_f"))
+                funcs.eval_f_grad_f = member_caller<&P::eval_f_grad_f>();
+            if (py::hasattr(o, "eval_f_g"))
+                funcs.eval_f_g = member_caller<&P::eval_f_g>();
+            if (py::hasattr(o, "eval_grad_f_grad_g_prod"))
+                funcs.eval_grad_f_grad_g_prod = member_caller<&P::eval_grad_f_grad_g_prod>();
+            if (py::hasattr(o, "eval_grad_L"))
+                funcs.eval_grad_L = member_caller<&P::eval_grad_L>();
+            if (py::hasattr(o, "eval_ψ"))
+                funcs.eval_ψ = member_caller<&P::eval_ψ>();
+            if (py::hasattr(o, "eval_grad_ψ"))
+                funcs.eval_grad_ψ = member_caller<&P::eval_grad_ψ>();
+            if (py::hasattr(o, "eval_ψ_grad_ψ"))
+                funcs.eval_ψ_grad_ψ = member_caller<&P::eval_ψ_grad_ψ>();
+            if (py::hasattr(o, "eval_f_grad_f"))
+                funcs.eval_f_grad_f = member_caller<&P::eval_f_grad_f>();
+            if (py::hasattr(o, "eval_hess_L_prod"))
+                funcs.eval_hess_L_prod = member_caller<&P::eval_hess_L_prod>();
+            if (py::hasattr(o, "eval_hess_L"))
+                funcs.eval_hess_L = member_caller<&P::eval_hess_L>();
+            if (py::hasattr(o, "eval_hess_ψ_prod"))
+                funcs.eval_hess_ψ_prod = member_caller<&P::eval_hess_ψ_prod>();
+            if (py::hasattr(o, "eval_hess_ψ"))
+                funcs.eval_hess_ψ = member_caller<&P::eval_hess_ψ>();
+            if (py::hasattr(o, "C"))
+                funcs.initialize_box_C = member_caller<&P::initialize_box_C>();
+            if (py::hasattr(o, "D"))
+                funcs.initialize_box_D = member_caller<&P::initialize_box_D>();
+            if (py::hasattr(o, "l1_reg"))
+                funcs.initialize_l1_reg = member_caller<&P::initialize_l1_reg>();
+            // clang-format on
+        } catch (py::error_already_set &e) {
+            // Needs to be caught between the interpreter is destroyed
+            throw python_loader_error(e.what());
+        }
+    }
+
+    cmvec vecn(const real_t *x) const { return cmvec{x, funcs.n}; }
+    mvec vecn(real_t *x) const { return mvec{x, funcs.n}; }
+    mindexvec vecn(index_t *x) const { return mindexvec{x, funcs.n}; }
+    cmvec vecm(const real_t *x) const { return cmvec{x, funcs.m}; }
+    mvec vecm(real_t *x) const { return mvec{x, funcs.m}; }
+
+    template <class... Args>
+    decltype(auto) call_func(const char *name, Args &&...args) {
+        py::gil_scoped_acquire gil;
+        try {
+            return o.attr(name)(std::forward<Args>(args)...);
+        } catch (py::error_already_set &e) {
+            throw python_loader_error(e.what());
+        }
+    }
+
+    /// Cost function.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_f()
+    real_t eval_f(const real_t *x) {
+        return py::cast<real_t>(call_func("eval_f", vecn(x)));
+    }
+    /// Gradient of the cost function.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_grad_f()
+    void eval_grad_f(const real_t *x, real_t *grad_fx) {
+        call_func("eval_grad_f", vecn(x), vecn(grad_fx));
+    }
+    /// Constraints function.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_g()
+    void eval_g(const real_t *x, real_t *gx) {
+        call_func("eval_g", vecn(x), vecm(gx));
+    }
+    /// Gradient-vector product of the constraints function.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_grad_g_prod()
+    void eval_grad_g_prod(const real_t *x, const real_t *y, real_t *grad_gxy) {
+        call_func("eval_grad_g_prod", vecn(x), vecm(y), vecn(grad_gxy));
+    }
+
+    /// Difference between point and its projection onto the general constraint
+    /// set D.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_proj_diff_g()
+    void eval_proj_diff_g(const real_t *z, real_t *e) {
+        call_func("eval_proj_diff_g", vecm(z), vecm(e));
+    }
+    /// Project the Lagrange multipliers.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_proj_multipliers()
+    void eval_proj_multipliers(real_t *y, real_t M) {
+        call_func("eval_proj_multipliers", vecm(y), M);
+    }
+    /// Proximal gradient step.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_prox_grad_step()
+    /// If not set, the default implementation from
+    /// @ref alpaqa::BoxConstrProblem is used.
+    real_t eval_prox_grad_step(real_t γ, const real_t *x, const real_t *grad_ψ,
+                               real_t *x̂, real_t *p) {
+        return py::cast<real_t>(call_func("eval_prox_grad_step", γ, vecn(x),
+                                          vecn(grad_ψ), vecn(x̂), vecn(p)));
+    }
+    /// Active indices for proximal operator.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_inactive_indices_res_lna()
+    /// If not set, the default implementation from
+    /// @ref alpaqa::BoxConstrProblem is used.
+    index_t eval_inactive_indices_res_lna(real_t γ, const real_t *x,
+                                          const real_t *grad_ψ, index_t *J) {
+        return py::cast<index_t>(call_func("eval_inactive_indices_res_lna", γ,
+                                           vecn(x), vecn(grad_ψ), vecn(J)));
+    }
+
+    /// Jacobian of the constraints function.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_jac_g()
+    void eval_jac_g(const real_t *x, real_t *J_values) {
+        call_func("eval_jac_g", vecn(x), mmat{J_values, funcs.m, funcs.n});
+    }
+#if 0
+    /// Get the sparsity pattern of the Jacobian of the constraints function.
+    /// @see @ref alpaqa::TypeErasedProblem::get_jac_g_sparsity()
+    alpaqa_sparsity_t get_jac_g_sparsity() {
+        // TODO: support sparse matrices
+    }
+#endif
+    /// Gradient of specific constraint function.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_grad_gi()
+    void eval_grad_gi(const real_t *x, index_t i, real_t *grad_gi) {
+        call_func("eval_grad_gi", vecn(x), i, vecn(grad_gi));
+    }
+    /// Hessian-vector product of the Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_hess_L_prod()
+    void eval_hess_L_prod(const real_t *x, const real_t *y, real_t scale,
+                          const real_t *v, real_t *Hv) {
+        call_func("eval_hess_L_prod", vecn(x), vecm(y), scale, vecn(v),
+                  vecn(Hv));
+    }
+    /// Hessian of the Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_hess_L()
+    void eval_hess_L(const real_t *x, const real_t *y, real_t scale,
+                     real_t *H_values) {
+        call_func("eval_hess_L", vecn(x), vecm(y), scale,
+                  mmat{H_values, funcs.n, funcs.n});
+    }
+#if 0
+    /// Get the sparsity pattern of the Hessian of the Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::get_hess_L_sparsity()
+    alpaqa_sparsity_t get_hess_L_sparsity() {
+        // TODO: support sparse matrices
+    }
+#endif
+    /// Hessian-vector product of the augmented Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_hess_ψ_prod()
+    void eval_hess_ψ_prod(const real_t *x, const real_t *y, const real_t *Σ,
+                          real_t scale, [[maybe_unused]] const real_t *zl,
+                          [[maybe_unused]] const real_t *zu, const real_t *v,
+                          real_t *Hv) {
+        call_func("eval_hess_ψ_prod", vecn(x), vecm(y), vecm(Σ), scale, vecn(v),
+                  vecn(Hv));
+    }
+    /// Hessian of the augmented Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_hess_ψ()
+    void eval_hess_ψ(const real_t *x, const real_t *y, const real_t *Σ,
+                     real_t scale, [[maybe_unused]] const real_t *zl,
+                     [[maybe_unused]] const real_t *zu, real_t *H_values) {
+        call_func("eval_hess_ψ", vecn(x), vecm(y), vecm(Σ), scale,
+                  mmat{H_values, funcs.n, funcs.n});
+    }
+#if 0
+    /// Get the sparsity pattern of the Hessian of the augmented Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::get_hess_ψ_sparsity()
+    alpaqa_sparsity_t get_hess_ψ_sparsity() {
+        // TODO: support sparse matrices
+    }
+#endif
+
+    /// Cost and its gradient.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_f_grad_f()
+    real_t eval_f_grad_f(const real_t *x, real_t *grad_fx) {
+        return py::cast<real_t>(
+            call_func("eval_f_grad_f", vecn(x), vecn(grad_fx)));
+    }
+    /// Cost and constraints.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_f_g()
+    real_t eval_f_g(const real_t *x, real_t *g) {
+        return py::cast<real_t>(call_func("eval_f_g", vecn(x), vecm(g)));
+    }
+    /// Gradient of the cost and gradient-vector product of the constraints.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_grad_f_grad_g_prod()
+    void eval_grad_f_grad_g_prod(const real_t *x, const real_t *y,
+                                 real_t *grad_f, real_t *grad_gxy) {
+        call_func("eval_grad_f_grad_g_prod", vecn(x), vecm(y), vecn(grad_f),
+                  vecn(grad_gxy));
+    }
+    /// Gradient of the Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_grad_L()
+    void eval_grad_L(const real_t *x, const real_t *y, real_t *grad_L,
+                     real_t *work_n) {
+        call_func("eval_grad_L", vecn(x), vecm(y), vecn(grad_L), vecn(work_n));
+    }
+
+    /// Augmented Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_ψ()
+    real_t eval_ψ(const real_t *x, const real_t *y, const real_t *Σ,
+                  [[maybe_unused]] const real_t *zl,
+                  [[maybe_unused]] const real_t *zu, real_t *ŷ) {
+        return py::cast<real_t>(
+            call_func("eval_ψ", vecn(x), vecm(y), vecm(Σ), vecm(ŷ)));
+    }
+    /// Gradient of the augmented Lagrangian.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_grad_ψ()
+    void eval_grad_ψ(const real_t *x, const real_t *y, const real_t *Σ,
+                     [[maybe_unused]] const real_t *zl,
+                     [[maybe_unused]] const real_t *zu, real_t *grad_ψ,
+                     real_t *work_n, real_t *work_m) {
+        call_func("eval_grad_ψ", vecn(x), vecm(y), vecm(Σ), vecn(grad_ψ),
+                  vecn(work_n), vecn(work_m));
+    }
+    /// Augmented Lagrangian and its gradient.
+    /// @see @ref alpaqa::TypeErasedProblem::eval_ψ_grad_ψ()
+    real_t eval_ψ_grad_ψ(const real_t *x, const real_t *y, const real_t *Σ,
+                         [[maybe_unused]] const real_t *zl,
+                         [[maybe_unused]] const real_t *zu, real_t *grad_ψ,
+                         real_t *work_n, real_t *work_m) {
+        return py::cast<real_t>(call_func("eval_ψ_grad_ψ", vecn(x), vecm(y),
+                                          vecm(Σ), vecn(grad_ψ), vecn(work_n),
+                                          vecn(work_m)));
+    }
+
+    /// Provide the initial values for the bounds of
+    /// @ref alpaqa::BoxConstrProblem::C, i.e. the constraints on the decision
+    /// variables.
+    void initialize_box_C(real_t *lb, real_t *ub) {
+        py::gil_scoped_acquire gil;
+        try {
+            auto C    = py::cast<py::tuple>(o.attr("C"));
+            auto C_lb = py::cast<crvec>(C[0]);
+            auto C_ub = py::cast<crvec>(C[1]);
+            if (C_lb.size() != funcs.n || C_ub.size() != funcs.n)
+                throw py::value_error("Invalid dimensions box C");
+            vecn(lb) = C_lb;
+            vecn(ub) = C_ub;
+        } catch (py::error_already_set &e) {
+            throw python_loader_error(e.what());
+        }
+    }
+    /// Provide the initial values for the bounds of
+    /// @ref alpaqa::BoxConstrProblem::D, i.e. the general constraints.
+    void initialize_box_D(real_t *lb, real_t *ub) {
+        py::gil_scoped_acquire gil;
+        try {
+            auto D    = py::cast<py::tuple>(o.attr("D"));
+            auto D_lb = py::cast<crvec>(D[0]);
+            auto D_ub = py::cast<crvec>(D[1]);
+            if (D_lb.size() != funcs.n || D_ub.size() != funcs.n)
+                throw py::value_error("Invalid dimensions box D");
+            vecn(lb) = D_lb;
+            vecn(ub) = D_ub;
+        } catch (py::error_already_set &e) {
+            throw python_loader_error(e.what());
+        }
+    }
+    /// Provide the initial values for @ref alpaqa::BoxConstrProblem::l1_reg,
+    /// the ℓ₁-regularization factor.
+    /// This function is called twice:
+    ///  1. with @p lambda set to `nullptr`, and the user should set the size.
+    ///  2. with @p lambda pointing to an array of that size, and the user
+    ///     should initialize this array.
+    void initialize_l1_reg(real_t *lambda, alpaqa_length_t *size) {
+        py::gil_scoped_acquire gil;
+        try {
+            if (lambda) {
+                auto lambda_py = py::cast<crvec>(o.attr("l1_reg"));
+                if (lambda_py.size() != *size)
+                    throw py::value_error("Invalid dimensions l1_reg");
+                mvec{lambda, *size} = lambda_py;
+            } else {
+                auto lambda_py = py::cast<crvec>(o.attr("l1_reg"));
+                *size          = lambda_py.size();
+            }
+        } catch (py::error_already_set &e) {
+            throw python_loader_error(e.what());
+        }
+    }
+};
+
+} // namespace
+
+} // namespace alpaqa
+
+/// Main entry point of this file, it is called by the
+/// @ref alpaqa::dl::DLProblem class.
+extern "C" PYTHON_LOADER_EXPORT alpaqa_problem_register_t
+register_alpaqa_problem(alpaqa_register_arg_t user_data_v) noexcept try {
+    // Check and convert user arguments
+    if (!user_data_v.data)
+        throw std::invalid_argument("Missing user data");
+    if (user_data_v.type != alpaqa_register_arg_strings)
+        throw std::invalid_argument("Invalid user data type");
+    using param_t    = std::span<std::string_view>;
+    const auto &opts = *reinterpret_cast<param_t *>(user_data_v.data);
+    // Python module and reference to load
+    std::string_view ref;
+    alpaqa::params::set_params(ref, "ref", opts);
+    if (ref.empty())
+        throw std::invalid_argument(
+            "Missing option problem.ref=package.module:Class");
+    // Build and expose problem
+    auto problem = std::make_unique<alpaqa::Problem>(ref, opts);
+    alpaqa_problem_register_t result;
+    result.functions = &problem->funcs;
+    result.instance  = problem.release();
+    result.cleanup   = [](void *instance) {
+        delete static_cast<alpaqa::Problem *>(instance);
+    };
+    return result;
+} catch (...) {
+    return {.exception = new alpaqa_exception_ptr_t{std::current_exception()}};
+}
+
+/// Returns the alpaqa DL ABI version. This version is verified for
+/// compatibility by the @ref alpaqa::dl::DLProblem constructor before
+/// registering the problem.
+extern "C" PYTHON_LOADER_EXPORT alpaqa_dl_abi_version_t
+register_alpaqa_problem_version() {
+    return ALPAQA_DL_ABI_VERSION;
+}
